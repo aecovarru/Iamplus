0f19e7c4b7b8bf5a168bc5713d1e5a975d82919c
Alan Covarrubias <aecovarru@gmail.com>
2016-06-28 19:15:16 -0700

    Add Bootstrap CSS

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,14 +6,14 @@ class UserController < ApplicationController
     auth_hash["info"].merge!({ "id" => auth_hash["uid"] })
     spotify_user = RSpotify::User.new(auth_hash)
 
-    user = User.find_or_create_by(user_id: spotify_user.id.to_i)
+    @user = User.find_or_create_by(user_id: spotify_user.id.to_i)
     playlists = spotify_user.playlists.delete_if { |playlist| playlist.owner.id != spotify_user.id }
     tracks = playlists.map { |playlist| playlist.tracks }.flatten
     artists = tracks.map { |track| track.artists }.flatten
     genres = artists.map { |artist| artist.genres }.flatten
 
-    user.create_artists(artists)
-    user.create_genres(genres)
+    @user.create_artists(artists)
+    @user.create_genres(genres)
 
   end
   
